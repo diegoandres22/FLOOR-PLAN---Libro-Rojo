806611b2a4590890a2b3ef22fd208cadaedde985
diegoandres22 <diego.a.v3005@gmail.com>
2024-11-13 16:16:32 -0400

    FIX[alertscreeen, notifications sents]: terminada bandeja de notificaciones enviadas

--- src/components/screen/AlertsScreen.tsx
+++ src/components/screen/AlertsScreen.tsx
@@ -4,12 +4,26 @@ import { Card, CardBody, Tab, Tabs } from '@nextui-org/react'
 import { NewNotification, NotificationsSents } from '../sections'
 
 export const AlertsScreen = () => {
+    const data = [
+        { id: '1', titulo: 'Nuevo producto disponible', texto: 'Ya Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt esse eius aspernatur, repudiandae eos molestias illum blanditiis officiis qui amet magnam quasi iusto, labore corrupti adipisci ratione optio. Nihil, cumque? disponible el nuevo sabor de helado', fecha: '2024-11-12' },
+        { id: '2', titulo: 'Nuevo producto disponible', texto: 'Ya tenemos disponible el nuevo sabor de helado', fecha: '2024-11-12' },
+        { id: '3', titulo: 'Nuevo producto disponible', texto: 'Ya Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt esse eius aspernatur, repudiandae eos molestias illum blandit labore corrupti adipisci ratione optio. Nihil, cumque? disponible el nuevo sabor de helado', fecha: '2024-11-12' },
+        { id: '4', titulo: 'Promoción de temporada', texto: 'Aprovecha lorem ofertas de otoño', fecha: '2024-11-10' },
+        { id: '5', titulo: 'Nuevo producto disponible', texto: 'Ya tenemos disponible el nuevo sabor de helado', fecha: '2024-11-12' },
+        { id: '6', titulo: 'Nuevo producto disponible', texto: 'Ya Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt esse eius aspernatur, repudiandae eos molestias illum blanditiis officiis qui amet magnam quasi iusto, labore corrupti adipisci ratione optio. Nihil, cumque? disponible el nuevo sabor de helado', fecha: '2024-11-12' },
+        { id: '7', titulo: 'Promoción de temporada', texto: 'Aprovecha lorem ofertas de otoño', fecha: '2024-11-10' },
+        { id: '8', titulo: 'Nuevo producto disponible', texto: 'Ya tenemos disponible el nuevo sabor de helado', fecha: '2024-11-12' },
+        { id: '9', titulo: 'Nuevo producto disponible', texto: 'Ya Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt esse eius aspernatur, repudiandae eos molestias illum blanditiis officiis qui amet magnam quasi iusto, labore corrupti adipisci ratione optio. Nihil, cumque? disponible el nuevo sabor de helado', fecha: '2024-11-12' },
+    ];
+
     return (
-        <Tabs aria-label="Options" className='flex justify-center mt-4'>
+        <Tabs aria-label="Options" className='flex justify-center items-center mt-4 '>
             <Tab key="sends" title="Notificaciones enviadas">
-                <Card>
-                    <CardBody>
-                        <NotificationsSents />
+                <Card className='max-w-[1000px] m-auto'>
+                    <CardBody className='h-[76vh] bg-neutral-200'>
+                        <h3 className="text-lg font-semibold ml-6">Todas las notificaciones</h3>
+
+                        <NotificationsSents notifications={data} />
                     </CardBody>
                 </Card>
             </Tab>
--- src/components/sections/AlertsSection/NotificationsSents.tsx
+++ src/components/sections/AlertsSection/NotificationsSents.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
 
-export const NotificationsSents = () => {
-    return (
-        <div>
-            Todas las notificaciones
-        </div>
-    )
+'use client';
+
+import React from 'react';
+import { Card, CardBody, Divider, ScrollShadow } from '@nextui-org/react';
+
+interface Notification {
+    id: string;
+    titulo: string;
+    texto: string;
+    fecha: string;
 }
+
+interface NotificationsSentProps {
+    notifications: Notification[];
+}
+
+export const NotificationsSents: React.FC<NotificationsSentProps> = ({ notifications }) => {
+    return (
+            <ScrollShadow className="w-full h-full p-2 scroll-auto " hideScrollBar >
+
+                {notifications.length === 0 ? (
+                    <p>No hay notificaciones enviadas</p>
+                ) : (
+                    notifications.map((notification) => (
+                        <Card key={notification.id} className="m-2 ">
+                            <CardBody>
+                                <div className="flex justify-between items-center">
+                                    <p className="text-medium font-semibold text-black/70">{notification.titulo}</p>
+                                    <p className="text-[10px] text-neutral-700 ">{notification.fecha}</p>
+                                </div>
+                                <p className="text-xs text-black/70">{notification.texto}</p>
+                            </CardBody>
+                        </Card>
+                    ))
+                )}
+            </ScrollShadow>
+    );
+};
+
